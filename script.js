ea071fd0a6c86763d96b0024e3625f0af3bfcf8f
doograssen <rythmag@gmail.com>
2023-08-11 00:34:32 +0300

    all task in one

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
 console.log('Hellow world');
-
-console.log('Запускаем таймер');
-alert('6');
-alert('5');
-alert('4');
-alert('3');
-alert('2');
-alert('1');
-console.log('Обратный отсчет закончен');
